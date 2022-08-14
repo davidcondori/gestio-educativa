afec59e626ef1a930c63615e99061bc44ec34072
davidcondori <deycon.22.adm@gmail.com>
2022-08-14 19:53:24 -0400

    first commit

---
+++ public_html/config/global.php
@@ -0,0 +1,21 @@
+<?php 
+//ip de la pc servidor base de datos
+define("DB_HOST", "localhost");
+
+// nombre de la base de datos
+define("DB_NAME", "u154694182_myproyecto");
+
+
+//nombre de usuario de base de datos
+define("DB_USERNAME", "u154694182_myproyecto");
+
+//conraseña del usuario de base de datos
+define("DB_PASSWORD", "Carolina2022%$");
+
+//codificacion de caracteres
+define("DB_ENCODE", "utf8");
+
+//nombre del proyecto
+define("PRO_NOMBRE", "myproyecto");
+ 
+ ?> 
